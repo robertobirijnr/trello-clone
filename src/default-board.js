3a7806563bee0153257cd6d357dafa0c34373c3e
robertobirijnr <robertobirijnr@hotmail.com>
2020-09-10 22:54:16 +0000

    board

---
+++ src/default-board.js
@@ -0,0 +1,48 @@
+import {
+  uuid
+} from './utils'
+
+export default {
+  name: 'workshop',
+  columns: [{
+    name: 'todo',
+    tasks: [{
+      description: 'I have to look into the requirements again then write them',
+      name: 'first task',
+      id: uuid(),
+      userAssigned: null
+    },
+    {
+      description: '',
+      name: 'second task',
+      id: uuid(),
+      userAssigned: null
+    },
+    {
+      description: '',
+      name: 'and thrid',
+      id: uuid(),
+      userAssigned: null
+    }
+    ]
+  },
+  {
+    name: 'in-progress',
+    tasks: [{
+      description: '',
+      name: 'first task',
+      id: uuid(),
+      userAssigned: null
+    }]
+  },
+  {
+    name: 'done',
+    tasks: [{
+      description: '',
+      name: 'first task',
+      id: uuid(),
+      userAssigned: null
+    }]
+  }
+  ]
+}
---
+++ src/views/Board.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="board">
+    <div class="flex flex-row item-start">
+      <div class="column" v-for="column of board.columns" :key="column.id">
+        <div class="flext items-center mb-2 font-bold">{{column.name}}</div>
+        <div class="list-reset">
+          <div class="task" v-for="task of column.tasks" :key="task.id">
+            <span class="w-full flex-no-shrink font-bold">{{task.name}}</span>
+            <p
+              v-if="task.description"
+              class="w-full flext-no-shrink mt-1 text-sm"
+            >{{task.description}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: mapState(["board"]),
+};
+</script>
+
+<style lang="css">
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+
+.column {
+  @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
+  min-width: 350px;
+}
+
+.board {
+  @apply p-4 bg-teal-dark h-full overflow-auto;
+}
+
+.task-bg {
+  @apply pin absolute;
+  background: rgba(0, 0, 0, 0.5);
+}
+</style>
